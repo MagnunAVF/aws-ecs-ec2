e9725fc3632bc456353ec6e76a5c1af70e569f46
MagnunAVF <magnunavf@gmail.com>
2025-05-13 04:21:19 -0300

    Create capacity providers

--- asg.tf
+++ asg.tf
@@ -29,3 +29,17 @@ resource "aws_autoscaling_group" "on_demand" {
   }
 
 }
+
+resource "aws_ecs_capacity_provider" "on_demand" {
+  name = format("%s-on-demand", var.project_name)
+
+  auto_scaling_group_provider {
+    auto_scaling_group_arn = aws_autoscaling_group.on_demand.arn
+    managed_scaling {
+      maximum_scaling_step_size = 10
+      minimum_scaling_step_size = 1
+      status                    = "ENABLED"
+      target_capacity           = 90
+    }
+  }
+}
